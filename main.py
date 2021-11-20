c523c055956975a69978d313ac3b61fabc0af8ab
Sovem Jung Bharati <88200119+hectic-sovembharati@users.noreply.github.com>
2021-11-20 17:50:10 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Welcome to Python Login CLI")
+name = input("Enter the correct name : ")
+password = input("Enter the Password : ")
+for i in name :
+    
+
+    if name == 'hello': #your name over here in username
+        pass
+    else:
+        print("Username Incorrect")
+   
+    
+
+    if password == 'hello': #your password over here
+    
+        print("Login Sucessfull")
+    
+    else:
+        print('Login Unsucessfull')
+        break
